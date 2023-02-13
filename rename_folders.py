7a5afd0afbf2dbb8e78613d570d1c408872ea45f
Adrien Lefevre <adrien.lefevre@edu.devinci.fr>
2023-02-13 15:38:40 +0100

    Adding 80 seq weigths and full rename_folders file

--- rename_folders.py
+++ rename_folders.py
@@ -1,36 +1,6 @@
 import json
 import os
 import math
-
-
-DATA_FOLDER = 'dataset/MP_Data'
-SUBFOLDERS = ['Train', 'Valid', 'Test']
-
-PREVIOUS_SEQ_NB = 30
-NEXT_SEQ_NB = 90
-TRAIN_RATIO = int(math.ceil((NEXT_SEQ_NB - PREVIOUS_SEQ_NB)*80/100))
-TEST_RATIO = int(math.ceil((NEXT_SEQ_NB - PREVIOUS_SEQ_NB)*10/100))
-
-def fix_data():
-    for subfolder in SUBFOLDERS:
-        if subfolder == 'Train': ratio = TRAIN_RATIO
-        else: ratio = TEST_RATIO
-        actions = os.listdir(os.path.join(DATA_FOLDER, subfolder))
-        for action in actions:
-            folders = os.listdir(os.path.join(DATA_FOLDER, subfolder, action))
-            for folder in folders:
-                folder_nb = int(folder.replace('.json', ''))
-                os.rename(os.path.join(DATA_FOLDER, subfolder, action, folder), os.path.join(DATA_FOLDER, subfolder, action, str(folder_nb+ratio)))
-                    
-    print("Done!")
-
-if __name__ == '__main__':
-    fix_data()
-
-
-import json
-import os
-import math
 import shutil
 
 
@@ -52,11 +22,12 @@ def fix_data():
             folders = os.listdir(os.path.join(PREVIOUS_DATA_FOLDER, subfolder, action))
             for folder in folders:
                 folder_nb = int(folder.replace('.json', ''))
-                os.rename(os.path.join(PREVIOUS_DATA_FOLDER, subfolder, action, folder), os.path.join(NEXT_DATA_FOLDER, subfolder, action, str(folder_nb+ratio)))
-                files = os.listdir(os.path.join(PREVIOUS_DATA_FOLDER, subfolder, action, str(folder_nb+ratio)))
+                os.mkdir(os.path.join(NEXT_DATA_FOLDER, subfolder, action, str(folder_nb+ratio)))
+                # os.rename(os.path.join(PREVIOUS_DATA_FOLDER, subfolder, action, folder), os.path.join(PREVIOUS_DATA_FOLDER, subfolder, action, str(folder_nb+ratio)))
+                files = os.listdir(os.path.join(PREVIOUS_DATA_FOLDER, subfolder, action, folder))
                 for file in files:
                     json_previous_file = os.path.join(PREVIOUS_DATA_FOLDER, subfolder, action, folder, file)
-                    json_next_file = os.path.join(NEXT_DATA_FOLDER, subfolder, action, folder, file)
+                    json_next_file = os.path.join(NEXT_DATA_FOLDER, subfolder, action, str(folder_nb+ratio), file)
                     if os.path.isfile(json_previous_file):
                         shutil.copy(json_previous_file, json_next_file)
 
